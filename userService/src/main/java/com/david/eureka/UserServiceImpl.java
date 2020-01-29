ea01cc30a4be9ec76f9017b84de3a1829a26c4d9
david <jgarcia.ext@aenor.com>
2020-01-29 15:25:47 +0100

    full working version

--- userService/src/main/java/com/david/eureka/UserServiceImpl.java
+++ userService/src/main/java/com/david/eureka/UserServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 public class UserServiceImpl implements UserService{
 
@@ -18,9 +20,25 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User registerUser(User input) {
+        if (!isValid(input.getUsername())) {
+            throw new IllegalArgumentException("Username must be an email");
+        }
         User createdUser = userRepository.save(input);
         sender.send(USER_CREATED_TOPIC, createdUser);
         return createdUser;
+    }
+
+    public boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
     }
 
     @Override
